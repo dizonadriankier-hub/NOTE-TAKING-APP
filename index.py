de06ee882e4eb4f14cc5ad31d6fb86acbcdd568a
Ariel-Amoguis <arielamoguis7@gmail.com>
2026-08-15 06:26:57 +0000

    add function in index.py

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
 print("Welcome to simple taking notes")
+
+notes = []
+
+def add():
+    note = input("Enter notes: ")
+    notes.append(note)
+
+while True:
+    choice = int(input("1: To enter notes 2: To Exit 3: View Notes\n> "))
+    if choice == 1:
+        add()
+    elif choice == 3:
+        print(notes)
+    else:
+        break
