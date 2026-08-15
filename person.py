c42c1ac3346a7b141d7180fda84a94bc3e5aabb7
piers-prs <r2s9wbztn9@privaterelay.appleid.com>
2026-08-15 06:27:30 +0000

    added Edit

--- person.py
+++ person.py
@@ -1,3 +1,25 @@
+
+
+
+
+
+
+#EDIT
+note_id = int(input("Enter note ID to edit: "))
+new_title = input("Enter New Title: ")
+
+for note in notes:
+    if note["id"] == note_id:
+        note["title"] = new_title
+        print(f"Updated! (Original Author: {note['author']})")
+        break
+
+
+
+
+
+
+#VIEW PERSON
 def view_notes(notes):
     if len (notes) == 0:
        print("No notes available")
