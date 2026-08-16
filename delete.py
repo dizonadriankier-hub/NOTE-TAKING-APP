95102ffe90ccd55ef5a28170e537fdeeffdc30bc
dizonadriankier-hub <dizonadriankier@gmail.com>
2026-08-16 13:45:49 +0000

    Add delete note feature

--- delete.py
+++ delete.py
@@ -1,2 +1,11 @@
 #Create Delete Function
 
+def delete(notes):
+    note_id = int(input("Enter note ID to delete: > "))
+
+    if note_id in notes:
+        del notes[note_id]
+        print("Note Deleted!\n")
+
+    else:
+        print("Note ID not found!\n")
