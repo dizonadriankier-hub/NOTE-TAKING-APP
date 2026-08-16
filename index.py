63793af3d3bb696f12c273b8d7a37add211c306c
dizonadriankier-hub <dizonadriankier@gmail.com>
2026-08-16 14:16:23 +0000

    Combined all note features

--- index.py
+++ index.py
@@ -1,16 +1,50 @@
-print("Welcome to simple taking notes")
+print("Welcome to Simple Note Taking\n")
 
-notes = []
+from edit import edit
+from person import view_notes
+from delete import delete
+
+notes = {}
+
 
 def add():
-    note = input("Enter notes: ")
-    notes.append(note)
+    note_id = int(input("Enter note ID: > "))
+    author = input("Enter author: > ")
+    note = input("Enter note: > ")
+
+    notes[note_id] = {
+        "author": author,
+        "note": note
+    }
+
+    print("Note Added!\n")
+
 
 while True:
-    choice = int(input("1: To enter notes 2: To Exit 3: View Notes\n> "))
+    choice = int(input(
+        "1: Add Notes\n"
+        "2: Exit\n"
+        "3: View Notes\n"
+        "4: Edit Notes\n"
+        "5: Delete Notes\n"
+        "> "
+    ))
+
     if choice == 1:
         add()
-    elif choice == 3:
-        print(notes)
-    else:
+
+    elif choice == 2:
+        print("Goodbye!")
         break
+
+    elif choice == 3:
+        view_notes(notes)
+
+    elif choice == 4:
+        edit(notes)
+
+    elif choice == 5:
+        delete(notes)
+
+    else:
+        print("Invalid choice!\n")
