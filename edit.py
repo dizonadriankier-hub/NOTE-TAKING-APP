343e4483d370daa2750f82296d69234f2f008585
dizonadriankier-hub <dizonadriankier@gmail.com>
2026-08-16 13:49:30 +0000

    Add edit note feature

--- edit.py
+++ edit.py
@@ -1,8 +1,15 @@
-note_id = int(input("Enter note ID to edit: "))
-new_title = input("Enter New Title: ")
+def edit(notes):
+    note_id = int(input("Enter note ID to edit: > "))
 
-for note in notes:
-    if note["id"] == note_id:
-        note["title"] = new_title
-        print(f"Updated! (Original Author: {note['author']})")
-        break
+    if note_id in notes:
+        print(f"Current note: {notes[note_id]['note']}")
+        print(f"Current author: {notes[note_id]['author']}")
+
+        new_note = input("Enter new note: > ")
+
+        notes[note_id]["note"] = new_note
+
+        print("Note Edited!\n")
+
+    else:
+        print("Note ID not found!\n")
